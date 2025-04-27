1c93a6b034c3cf831e40b35d33961392472d2a8d
Nguyen Tan Duy <duynguyen1918@gmail.com>
2025-04-27 22:02:25 +0700

    Cap nhat home.php

--- views/home.php
+++ views/home.php
@@ -1,16 +1,25 @@
 <!DOCTYPE html>
 <html lang="vi">
+
 <head>
     <meta charset="UTF-8">
     <title>Trang kiểm thử hệ thống</title>
 </head>
+
 <body>
     <h1>Chào mừng đến với Porsche Store (PHP MVC)</h1>
     <h2>Chọn chức năng để kiểm thử:</h2>
     <ul>
         <li><a href="index.php?controller=CarAdmin&action=index">Xem danh sách xe</a></li>
-      
+        <li><a href="index.php?controller=User&action=login">Dang nhập</a></li>
+        <li><a href="index.php?controller=User&action=register">Dang kí</a></li>
+        <li><a href="index.php?controller=Product&action=Details&id=1">Car details</a></li>
+
+
+
+
         <!-- Thêm link khác nếu cần -->
     </ul>
 </body>
+
 </html>

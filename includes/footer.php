a3e4b86a7d4b6f0d7cee623d5d5e17436f0cd5c1
NgBPhu <thanhtien12a2q@gmail.com>
2025-04-27 21:38:25 +0700

    Update footer.php

---
+++ includes/footer.php
@@ -0,0 +1,65 @@
+<?php
+// includes/footer.php
+?>
+    <!-- ==================== FOOTER ==================== -->
+    <footer class="footer-dark pt-5 pb-4">
+        <div class="container">
+            <div class="row">
+                <!-- Cột 1: Logo và Thông tin -->
+                <div class="col-md-4 col-lg-3 mb-4">
+                     <h5 class="text-uppercase mb-4">
+                         <strong>PORSCHE</strong> <span style="font-size: 0.7em; vertical-align: super;">VIETNAM</span>
+                     </h5>
+                    <p>
+                        Porsche Việt Nam<br>
+                        Số 666 Đường ABC<br>
+                        Quận 1, TP.HCM
+                    </p>
+                </div>
+
+                <!-- Cột 2: Liên kết nhanh -->
+                <div class="col-md-2 col-lg-2 mb-4">
+                    <h5>Liên kết nhanh</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="index.php">Trang chủ</a></li>
+                        <li><a href="#">Dòng xe</a></li>
+                        <li><a href="#">Giới thiệu</a></li>
+                        <li><a href="#">Tin tức</a></li>
+                    </ul>
+                </div>
+
+                <!-- Cột 3: Dịch vụ -->
+                <div class="col-md-3 col-lg-2 mb-4">
+                    <h5>Dịch vụ</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="#">Bảo dưỡng</a></li>
+                        <li><a href="#">Phụ tùng</a></li>
+                        <li><a href="#">Bảo hành</a></li>
+                        <li><a href="#">Tài chính</a></li>
+                    </ul>
+                </div>
+
+                <!-- Cột 4: Kết nối -->
+                <div class="col-md-3 col-lg-3 ms-lg-auto mb-4">
+                    <h5>Kết nối</h5>
+                    <p>Theo dõi chúng tôi:</p>
+                    <div>
+                        <a href="#" class="me-3 fs-4"><i class="bi bi-facebook"></i></a>
+                        <a href="#" class="me-3 fs-4"><i class="bi bi-instagram"></i></a>
+                        <a href="#" class="me-3 fs-4"><i class="bi bi-twitter"></i></a>
+                    </div>
+                </div>
+            </div>
+
+            <hr class="my-4">
+
+            <div class="text-center">
+                <p>© <?php echo date("Y"); ?> Porsche Vietnam. All Rights Reserved.</p> <?php ?>
+            </div>
+        </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+</body>
+</html>

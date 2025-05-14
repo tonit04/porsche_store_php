bcecab8744c1ad5c3434c45470931f227e5fa65c
Nguyen Tan Duy <duynguyen1918@gmail.com>
2025-05-14 14:33:25 +0700

    Lưu thay đổi User.php trước khi pull

--- models/User.php
+++ models/User.php
@@ -1,21 +1,25 @@
 <?php
 require_once __DIR__ . '/../config/Database.php';
 
-class User {
+class User
+{
     private $conn;
 
-    public function __construct() {
+    public function __construct()
+    {
         $db = new Database();
         $this->conn = $db->connect();
     }
 
-    public function findByUsername($username) {
+    public function findByUsername($username)
+    {
         $stmt = $this->conn->prepare("SELECT * FROM users WHERE username = :username LIMIT 1");
         $stmt->execute(['username' => $username]);
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
-    public function create($data) {
+    public function create($data)
+    {
         $stmt = $this->conn->prepare("INSERT INTO users 
             (username, password, email, full_name, phone, address, role, created_at, updated_at, is_verified)
             VALUES (:username, :password, :email, :full_name, :phone, :address, :role, NOW(), NOW(), 0)");
